f08dda786826a07ec74c0e4790583fe696baede9
Mason <lantay@alumni.unc.edu>
2017-07-18 14:36:33 -0700

    started implementing moose's stuff

--- env.sh
+++ env.sh
@@ -1,2 +1,8 @@
 export MONGODB_URI="mongodb://mason:lantay@ds157282.mlab.com:57282/earlystage"
 export BOT_TOKEN="xoxb-213893670865-P3C4cTI4BP2Gx9MK0BDq793R"
+export SLACK_OAUTH_TOKEN=""
+export API_AI_TOKEN=""
+export GOOGLE_CLIENT_ID=""
+export GOOGLE_CLIENT_SECRET=""
+export DOMAIN=""
+
